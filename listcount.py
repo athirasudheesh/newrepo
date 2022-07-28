1f972d4ad87116c654f4c641b4b8158833ac9fde
athira sudheesh <athirasudhi245@gmail.com>
2022-07-28 18:13:38 +0530

    my draw start program

--- listcount.py
+++ listcount.py
@@ -3,8 +3,7 @@ print("enter the numbers")
 for i in range(8):
     i=int(input())
     a.append(i)
-count_5=a.count(a[4])
-if count_5==3:
-    print("TRUE") 
+if a[4]==a[5]==a[6]:
+    print("TRUE")
 else:
     print("FALSE")    
